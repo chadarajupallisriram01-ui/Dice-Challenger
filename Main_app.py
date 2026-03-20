0bec9092951404692923139c8f45257a0cd307d7
chadarajupallisriram01-ui <chadarajupalli.sriram01@gmail.com>
2026-03-20 18:04:49 +0530

    Update Main_app.py

--- Main_app.py
+++ Main_app.py
@@ -45,8 +45,7 @@ if st.session_state.page == "Login":
         if mobile(Mobile) == "You are Registered Successfully..":
             st.checkbox("Mobile Number",True)
             st.session_state.page = "start"
-            st.rerun()
-
+            
 elif st.session_state.page == "start":
     st.subheader("Welcome to Dice Challenger")
     st.success("All the Best all my players 👍")
